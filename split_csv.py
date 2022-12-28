40765b51a953ba28c5e5332339a41cae84681d67
BRV4K <a.buravov13@mail.ru>
2022-12-28 22:31:21 +0500

    splitting csv by years docstring

--- split_csv.py
+++ split_csv.py
@@ -2,6 +2,12 @@ import csv
 
 
 def csv_reader():
+    '''
+    Функция считывет исходный csv файл, возваращет данные из файла, заголовки файла и словарь с годами
+    :return: data: массив с данными из файла,
+    name: массив с заголовками файла,
+    years: словарь с годами
+    '''
     with open(input('Введите название файла: '), encoding='utf-8-sig') as r_file:
         file = csv.reader(r_file, delimiter=",")
         count = 0
@@ -27,6 +33,13 @@ def csv_reader():
 data, name, years = csv_reader()
 
 def get_csv_by_years(data, name, years):
+    '''
+    Функия разбивает исходный файл на мелкие файлы по годам
+    :param data: массив с данными из файла
+    :param name: массив с заголовками файла
+    :param years: словарь с годами
+    После работы функции в папку 'split' сохраняются файлы по годам
+    '''
     for year in years.keys():
         with open(f'split/data_{year}.csv', 'w', encoding='utf-8-sig') as f:
             writer = csv.writer(f)
